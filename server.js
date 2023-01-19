b5f53e065b7aa5118c657c4c18ab9e8ea5f848cf
Apurva Shekhar <103213540+apurvashekhar96@users.noreply.github.com>
2023-01-19 12:31:55 +0530

    changed server.js

--- server.js
+++ server.js
@@ -26,25 +26,23 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use("/", (req, res, next) => {
-//   res.sendFile(
-//     path.join(__dirname, "portfolioappclient", "build", "index.html")
-//   );
-//   //next();
-// });
-
 app.use(configRoutes);
 
 app.use(formDataRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(
-      path.join(__dirname, "portfolioappclient/build", "index.html")
-    );
-  });
-}
+app.use("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "portfolioappclient/build", "index.html"));
+  next();
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function (req, res) {
+//     res.sendFile(
+//       path.join(__dirname, "portfolioappclient/build", "index.html")
+//     );
+//   });
+// }
 
 mongoose
   .connect(mongourl)
